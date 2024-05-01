bb35125f1a62e078b1aeecde9273627df1be71dc
Hubert Wiktor <hubertwiktor6@gmail.com>
2024-05-01 11:44:12 +0200

    test

--- src/app/context/AppContext.tsx
+++ src/app/context/AppContext.tsx
@@ -38,7 +38,6 @@ export const BalanceProvider: React.FC<{ children: React.ReactNode }> = ({ child
     return ;
   });
 
-  console.log(true)
   
   const [globalBalance, setGlobalBalance] = useState<number>(0)
 
